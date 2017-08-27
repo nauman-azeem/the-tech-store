2e52ab5f3d4799054a4606e7b835d78841dfb6e6
Nauman Azeem <borissims4@gmail.com>
2017-08-27 14:01:12 +0500

    Improvised Barrels, Nav Bar Directive initiated

--- src/app/directives/index.ts
+++ src/app/directives/index.ts
@@ -1,1 +1,2 @@
 export { DropdownDirective } from './dropdown.directive';
+export { NavBarCollapseDirective } from './nav-bar-collapse.directive';
---
+++ src/app/directives/nav-bar-collapse.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[ttsNavBarCollapse]'
+})
+export class NavBarCollapseDirective {
+
+  constructor() { }
+
+}
